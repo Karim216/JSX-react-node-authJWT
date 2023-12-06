b52c9cee4c13028a9ae9ef5b26966e816be23e06
Karim216 <njigbetnkom@gmail.com>
2023-12-06 16:58:27 +0100

    new update

--- api/src/config/db.config.js
+++ api/src/config/db.config.js
@@ -1,6 +1,16 @@
-module.exports = {
-  HOST: process.env.DB_HOST,
-  USER: process.env.DB_USER,
-  PASSWORD: process.env.DB_PASSWORD,
-  DB: process.env.DB_NAME,
-};
+// module.exports = {
+//   HOST: process.env.DB_HOST,
+//   USER: process.env.DB_USER,
+//   PASSWORD: process.env.DB_PASSWORD,
+//   DB: process.env.DB_NAME,
+// };
+
+// config.js
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('nom_de_la_base_de_donnees', 'utilisateur', 'mot_de_passe', {
+  host: 'localhost',
+  dialect: 'mysql',
+});
+
+module.exports = sequelize;
--- api/src/controllers/me.controller.js
+++ api/src/controllers/me.controller.js
@@ -20,26 +20,11 @@ exports.getUserInfo = async (req, res) => {
             return;
           }
 
-          // Utilisez await pour obtenir le résultat de getUserRight
-          const right = await getUserRight(user.profile_id);
-
           res.json({
               id: user.id,
               email: user.email,
               firstname: user.firstname,
               lastname: user.lastname,
-              country: user.country,
-              city: user.city,
-              fushoraire: user.fushoraire,
-              imgURL: user.imgURL,
-              idProfile: user.profile_id,
-              profileName: user.profileName,
-              typeressource: user.typeressource,
-              function: user.jobTitle,
-              serviceTeam: user.serviceTeam,
-              team: user.team,
-              right: right,
-              imgURL: user.imgURL,
               // You can include more user fields here
           });
         } catch (err) {
@@ -51,50 +36,4 @@ exports.getUserInfo = async (req, res) => {
   } else {
     res.sendStatus(403);
   }
-};
-
-
-// A revenir
-// exports.getUserId = async (req, res) => {
-
-//   const myToken = req.cookies['auth_token'];
-
-//   console.log(myToken)
-
-//   try {
-//     let decodedData = jwt.decode(myToken);
-//     if (!decodedData || !decodedData.email) {
-//       return null;
-//     }
-//     let user = await Auth.findUserByEmail(decodedData.email);
-//     if (!user) {
-//       res.status(404).send("User not found");
-//       return;
-//     }
-
-//     return user.id;
-
-//   } catch (err) {
-//     console.error(err);
-//     return null;
-//   }
-
-// };
-
-
-// Find a single Profile by Id
-const getUserRight = (id) => {
-  return new Promise((resolve, reject) => {
-    Me.findRightById(id, (err, data) => {
-      if (err) {
-        if (err.kind === "not_found") {
-          reject({ message: `Not found Profile with id ${id}.` });
-        } else {
-          reject({ message: "Error retrieving Profile with id " + id });
-        }
-      } else {
-        resolve(data);
-      }
-    });
-  });
 };
